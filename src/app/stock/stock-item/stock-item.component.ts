b46d0c2550029d4ba93d598eb59b4a8a06ef62c4
Weng <bishop781115@gmail.com>
2018-11-01 17:54:25 +0800

    新增價格顏色(漲紅跌綠) (參考P.23)

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -11,6 +11,7 @@ export class StockItemComponent implements OnInit {
   public code: string;
   public price: number;
   public previousPrice: number;
+  public positiveChang: boolean;
 
   constructor() { }
 
@@ -19,6 +20,7 @@ export class StockItemComponent implements OnInit {
     this.code = 'TSC';
     this.price = 85;
     this.previousPrice = 80;
+    this.positiveChang = this.price >= this.previousPrice;
   }
 
 }
