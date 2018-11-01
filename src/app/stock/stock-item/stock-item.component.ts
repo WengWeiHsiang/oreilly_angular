4b0e053aadfee25ef0a52f4facd61a15267afca3
Weng <bishop781115@gmail.com>
2018-11-01 16:54:22 +0800

    實作出書本P.21的內容

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -7,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockItemComponent implements OnInit {
 
-  title = 'stock-item is work!';
+  public name: string;
+  public code: string;
+  public price: number;
+  public previousPrice: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.name = 'Test Stock Company';
+    this.code = 'TSC';
+    this.price = 85;
+    this.previousPrice = 80;
   }
 
 }
