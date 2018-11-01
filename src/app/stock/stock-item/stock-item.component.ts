32423bdd0134e709707ab8d26819f4ed4e6f0d92
Weng <bishop781115@gmail.com>
2018-11-01 17:57:32 +0800

    錯字修正

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -19,7 +19,7 @@ export class StockItemComponent implements OnInit {
     this.name = 'Test Stock Company';
     this.code = 'TSC';
     this.price = 85;
-    this.previousPrice = 80;
+    this.previousPrice = 90;
     this.positiveChang = this.price >= this.previousPrice;
   }
 
