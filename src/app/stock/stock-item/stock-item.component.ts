4df5ff351586ef6bec2562620b8a66417ecc5c75
Weng <bishop781115@gmail.com>
2018-11-05 13:55:52 +0800

    ngFor 和 ngIf

--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -9,34 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
   public stockClasses;
   public stockStyles;
 
   constructor() { }
 
   ngOnInit() {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
-    const diff = this.stock.price / this.stock.previousPrice - 1;
-    const largeChange = Math.abs(diff) > 0.05;
-
-    this.stockStyles = {
-      'color': this.stock.isPositiveChange() ? 'red' : 'green',
-      'font-size': largeChange ? '1.2em' : '0.8em'
-    };
-
-    /*
-    this.stockClasses = {
-      'positive': this.stock.isPositiveChange(),
-      'negative': !this.stock.isPositiveChange(),
-      'large-change': largeChange,
-      'small-change': !largeChange
-    };
-    */
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80),
+      new Stock('Second Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 755),
+    ];
   }
 
-  toggleFavorite(event) {
-    console.log('We are toggling the favorite state for this stock', event);
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(event, index) {
+    console.log('We are toggling the favorite state for this stock', index, event);
+    this.stocks[index].favorite = !this.stocks[index].favorite;
   }
 }
