ed6cdb210ea928450c15ec98ea1a7f4b904bf6e8
Weng <bishop781115@gmail.com>
2018-11-09 15:01:39 +0800

    改用Input Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Stock } from './model/stock';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'oreilly-angular';
+
+export class AppComponent implements OnInit {
+
+  title = 'Oreilly Angular';
+
+  public stockObj: Stock;
+
+  ngOnInit(): void {
+    this.stockObj = new Stock('Test Stock', 'TS', 85, 80);
+  }
 }
--- src/app/stock/stock-item/stock-item.component.ts
+++ src/app/stock/stock-item/stock-item.component.ts
@@ -1,5 +1,5 @@
 import { Stock } from './../../model/stock';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -7,28 +7,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './stock-item.component.html',
   styleUrls: ['./stock-item.component.css']
 })
-export class StockItemComponent implements OnInit {
 
-  public stocks: Array<Stock>;
-  public stockClasses;
-  public stockStyles;
+export class StockItemComponent {
 
-  constructor() { }
+  @Input() public stock: Stock;
+  @Output() private toggleFavorite: EventEmitter<Stock>;
 
-  ngOnInit() {
-    this.stocks = [
-      new Stock('Test Stock Company', 'TSC', 85, 80),
-      new Stock('Second Stock Company', 'SSC', 10, 20),
-      new Stock('Last Stock Company', 'LSC', 876, 755),
-    ];
+  constructor() {
+    this.toggleFavorite = new EventEmitter<Stock>();
   }
 
-  toggleFavorite(event, index) {
-    console.log('We are toggling the favorite state for this stock', index, event);
-    this.stocks[index].favorite = !this.stocks[index].favorite;
-  }
-
-  trackStockByCode(index, stock) {
-    return stock.code;
+  onToggleFavorite(event) {
+    console.log('We are toggling the favorite state for this stock', event);
+    this.toggleFavorite.emit(this.stock);
   }
 }
